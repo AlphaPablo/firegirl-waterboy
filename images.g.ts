e4eabffbfeea64285ed41d440b12f4ce4eb30705
AlphaPablo <156251965+AlphaPablo@users.noreply.github.com>
2024-04-30 15:30:17 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,101 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`;
+            case "image2":
+            case "myImage0":return img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`;
+            case "image3":
+            case "myImage1":return img`
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+`;
+            case "image4":
+            case "myImage2":return img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,4 +1,359 @@
-let mySprite = sprites.create(img`
+namespace SpriteKind {
+    export const water = SpriteKind.create()
+    export const fire = SpriteKind.create()
+}
+controller.player2.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+	
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite2,
+    [img`
+        2 2 2 4 2 2 2 2 2 2 2 2 2 . . . 
+        . 4 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+        2 2 2 4 4 4 2 4 2 4 4 4 2 4 4 4 
+        2 4 4 4 4 4 4 4 4 4 4 4 4 2 4 4 
+        . 4 2 4 4 4 f 4 4 4 f 4 4 2 2 . 
+        . 2 2 4 4 4 4 4 4 4 4 4 4 . 2 2 
+        2 2 . 4 4 4 4 f f f 4 4 4 . . . 
+        . . . . . e e 4 4 4 4 e . . . . 
+        . . . . 4 4 e 4 4 4 e 4 4 4 . . 
+        . e e 4 4 . 4 4 4 4 4 4 . 4 . . 
+        e e 4 4 e . 4 4 4 4 4 4 . 4 4 . 
+        e e e e e e 4 4 4 4 4 4 . . . . 
+        e e e e e e 4 4 . . 4 4 . . . . 
+        . e e e e e 4 4 . . 4 4 . . . . 
+        . . e e e e 4 4 . . 4 4 . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . 2 2 2 2 2 2 . . . . . . . 
+        . . 2 2 5 5 5 5 5 5 . . . . . . 
+        2 2 2 2 5 5 5 2 2 4 . . . . . . 
+        . . 2 2 5 5 5 2 4 4 . . . . . . 
+        2 2 5 5 5 2 5 5 5 f 4 . . . . . 
+        5 5 5 2 5 5 5 5 5 4 f . . . . . 
+        5 2 2 2 5 5 5 2 2 f f . . . . . 
+        . 5 5 5 5 4 4 4 4 f . . . . . . 
+        . . 2 2 . 4 4 4 4 . . . . . . . 
+        . . . . . 4 4 4 . . . . . . . . 
+        . . . . . 4 4 4 . . 4 4 4 4 . . 
+        . . . . . 4 4 4 4 4 4 . . . . . 
+        . . . . . 4 4 4 . . . . . . . . 
+        . . . . . 4 4 4 . . . . . . . . 
+        . . . . . 4 4 4 . . . . . . . . 
+        . . . . . 4 4 4 . . . . . . . . 
+        `,img`
+        2 2 2 4 2 2 2 2 2 2 2 2 2 . . . 
+        . 4 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+        2 2 2 4 4 4 2 4 2 4 4 4 2 4 4 4 
+        2 4 4 4 4 4 4 4 4 4 4 4 4 2 4 4 
+        . 4 2 4 4 4 f 4 4 4 f 4 4 2 2 . 
+        . 2 2 4 4 4 4 4 4 4 4 4 4 . 2 2 
+        2 2 . 4 4 4 4 f f f 4 4 4 . . . 
+        . . . . . e e 4 4 4 4 e . . . . 
+        . . . . 4 4 e 4 4 4 e 4 4 4 . . 
+        . e e 4 4 . 4 4 4 4 4 4 . 4 . . 
+        e e 4 4 e . 4 4 4 4 4 4 . 4 4 . 
+        e e e e e e 4 4 4 4 4 4 . . . . 
+        e e e e e e 4 4 . . 4 4 . . . . 
+        . e e e e e 4 4 . . 4 4 . . . . 
+        . . e e e e 4 4 . . 4 4 . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    500,
+    false
+    )
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 6 6 6 . . . . . . . 
+        . . . . 6 6 6 6 6 6 . . . . . . 
+        . . . . 6 6 6 6 f 6 . . . . . . 
+        . . . . 6 6 6 6 6 6 . . . . . . 
+        . . . . 6 6 6 f f f . . . . . . 
+        . . . . 6 6 6 f f f . . . . . . 
+        . . . . 6 6 6 f f f . . . . . . 
+        . . . . 6 6 6 6 6 6 . . . . . . 
+        . . . . . 6 6 6 6 . . . . . . . 
+        . . . . . 6 6 6 6 6 . . . . . . 
+        . . . . . 6 6 6 6 . . . . . . . 
+        . . . . . . 6 6 . . . . . . . . 
+        . . . . . 6 6 . 6 . . . . . . . 
+        . . . . . 6 . . 6 . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 9 6 6 6 . . . 6 6 6 6 6 6 . 
+        . . . 6 6 6 6 6 6 6 6 6 6 6 6 . 
+        . . . 6 6 f f 6 6 6 6 f f 6 6 . 
+        . . . 6 6 6 6 f f f f 6 6 6 6 . 
+        . . . 9 9 9 6 6 6 6 6 6 6 6 6 . 
+        . . . 9 9 9 6 6 6 6 6 6 6 6 6 . 
+        . . . . . 9 9 6 6 6 6 6 6 9 9 . 
+        . . . 6 6 6 6 6 6 6 6 6 6 6 6 . 
+        8 8 8 6 8 . . 6 6 6 6 6 6 . 6 . 
+        8 8 8 8 8 8 8 6 6 6 6 6 . . 6 6 
+        8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+        8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+        8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+        `],
+    1000,
+    false
+    )
+    if (true) {
+        energy(mySprite)
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.vy == 0) {
+        mySprite.vy = -150
+    }
+})
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    if (mySprite2.vy == 0) {
+        energy(mySprite2)
+    }
+})
+scene.onOverlapTile(SpriteKind.fire, sprites.dungeon.chestClosed, function (sprite, location) {
+    info.changeScoreBy(100)
+    game.setGameOverMessage(true, "CONGRATS FIREGIRL WIN")
+    effects.confetti.endScreenEffect()
+})
+info.onScore(100, function () {
+    game.setGameOverEffect(true, effects.confetti)
+    game.gameOver(true)
+})
+scene.onOverlapTile(SpriteKind.fire, sprites.dungeon.hazardWater, function (sprite, location) {
+    info.changeLifeBy(-1)
+    game.setGameOverMessage(false, "GAME OVER!")
+})
+scene.onOverlapTile(SpriteKind.water, sprites.dungeon.hazardLava1, function (sprite, location) {
+    info.changeLifeBy(-1)
+    game.setGameOverMessage(false, "GAME OVER!")
+})
+info.onLifeZero(function () {
+    sprites.destroy(mySprite)
+    sprites.destroy(mySprite2)
+    game.gameOver(false)
+})
+function energy (mySprite: Sprite) {
+    for (let index = 0; index < 2; index++) {
+        for (let index = 0; index <= 3; index++) {
+            if (statusbar.value >= 10) {
+                statusbar.value += -5
+            } else if (statusbar.value >= 90) {
+                projeciel1 = list[index]
+            } else if (statusbar.value >= 99) {
+                projectile2 = list[index]
+            } else {
+                projectile3 = list[index]
+            }
+            if (statusbar.value <= 10) {
+                animation.runImageAnimation(
+                mySprite,
+                [img`
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . 9 6 6 6 . . . 6 6 6 6 6 6 . 
+                    . . . 6 6 6 6 6 6 6 6 6 6 6 6 . 
+                    . . . 6 6 f f 6 6 6 6 f f 6 6 . 
+                    . . . 6 6 6 6 f f f f 6 6 6 6 . 
+                    . . . 9 9 9 6 6 6 6 6 6 6 6 6 . 
+                    . . . 9 9 9 6 6 6 6 6 6 6 6 6 . 
+                    . . . . . 9 9 6 6 6 6 6 6 9 9 . 
+                    . . . 6 6 6 6 6 6 6 6 6 6 6 6 . 
+                    8 8 8 6 8 . . 6 6 6 6 6 6 . 6 . 
+                    8 8 8 8 8 8 8 6 6 6 6 6 . . 6 6 
+                    8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+                    8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+                    8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+                    `,img`
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . 9 6 6 6 . . . 6 6 6 6 6 6 . 
+                    . . . 6 6 6 6 6 6 6 6 6 6 6 6 . 
+                    . . . 6 6 f f 6 6 6 6 f f 6 6 . 
+                    . . . 6 6 6 6 f f f f 6 6 6 6 . 
+                    . . . 9 9 9 6 6 6 6 6 6 6 6 6 . 
+                    . . . 9 9 9 6 6 6 6 6 6 6 6 6 . 
+                    . . . . . 9 9 6 6 6 6 6 6 9 9 . 
+                    . . . 6 6 6 6 6 6 6 6 6 f . . . 
+                    8 8 8 6 8 . . 6 6 6 6 6 f . . . 
+                    8 8 8 8 8 8 8 6 6 6 6 6 f . . . 
+                    8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+                    8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+                    8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+                    `,img`
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . 9 6 6 6 . . . 6 6 6 6 6 6 . 
+                    . . . 6 6 6 6 6 6 6 6 6 6 6 6 . 
+                    . . . 6 6 f f 6 6 6 6 f f 6 6 . 
+                    . . . 6 6 6 6 f f f f 6 6 6 6 . 
+                    . . . 9 9 9 6 6 6 6 6 6 6 6 6 . 
+                    . . . 9 9 9 6 6 6 6 6 6 6 6 6 . 
+                    . . . . . 9 9 6 6 6 6 6 6 9 f . 
+                    . . . 6 6 6 6 6 6 6 6 6 6 f f . 
+                    8 8 8 6 8 . . 6 6 6 6 6 f f . . 
+                    8 8 8 8 8 8 8 6 6 6 6 6 . . . . 
+                    8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+                    8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+                    8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+                    `,img`
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . 9 6 6 6 . . . 6 6 6 6 6 6 . 
+                    . . . 6 6 6 6 6 6 6 6 6 6 6 6 . 
+                    . . . 6 6 f f 6 6 6 6 f f 6 6 . 
+                    . . . 6 6 6 6 f f f f 6 6 6 6 . 
+                    . . . 9 9 9 6 6 6 6 6 6 6 6 6 . 
+                    . . . 9 9 9 6 6 6 6 6 6 6 6 6 . 
+                    . . . . . 9 9 6 6 6 6 6 6 9 9 . 
+                    . . . 6 6 6 6 6 6 6 f f 6 . . . 
+                    8 8 8 6 8 . . 6 6 6 6 f f . . . 
+                    8 8 8 8 8 8 8 6 6 6 6 6 . . . . 
+                    8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+                    8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+                    8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
+                    `],
+                500,
+                false
+                )
+                animation.runImageAnimation(
+                mySprite2,
+                [img`
+                    2 2 2 4 2 2 2 2 2 2 2 2 2 . . . 
+                    . 4 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+                    2 2 2 4 4 4 2 4 2 4 4 4 2 4 4 4 
+                    2 4 4 4 4 4 4 4 4 4 4 4 4 2 4 4 
+                    . 4 2 4 4 4 f 4 4 4 f 4 4 2 2 . 
+                    . 2 2 4 4 4 4 4 4 4 4 4 4 . 2 2 
+                    2 2 . 4 4 4 4 f f f 4 4 4 . . . 
+                    . . . . . e e 4 4 4 4 e . . . . 
+                    . . . . 4 4 e 4 4 4 e 4 4 4 . . 
+                    . e e 4 4 . 4 4 4 4 4 4 . 4 . . 
+                    e e 4 4 e . 4 4 4 4 4 4 . 4 4 . 
+                    e e e e e e 4 4 4 4 4 4 . . . . 
+                    e e e e e e 4 4 . . 4 4 . . . . 
+                    . e e e e e 4 4 . . 4 4 . . . . 
+                    . . e e e e 4 4 . . 4 4 . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    `,img`
+                    2 2 2 4 2 2 2 2 2 2 2 2 2 . . . 
+                    . 4 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+                    2 2 2 4 4 4 2 4 2 4 4 4 2 4 4 4 
+                    2 4 4 4 4 4 4 4 4 4 4 4 4 2 4 4 
+                    . 4 2 4 4 4 f 4 4 4 f 4 4 2 2 . 
+                    . 2 2 4 4 4 4 4 4 4 4 4 4 . 2 2 
+                    2 2 . 4 4 4 4 f f f 4 4 4 . . . 
+                    . . . . . e e 4 4 4 4 e . . . . 
+                    . . . . 4 4 e 4 4 4 e 4 4 4 . 2 
+                    . e e 4 4 . 4 4 4 4 4 4 . 4 . 2 
+                    e e 4 4 e . 4 4 4 4 4 4 . 4 2 . 
+                    e e e e e e 4 4 4 4 4 4 . . . . 
+                    e e e e e e 4 4 . . 4 4 . . . . 
+                    . e e e e e 4 4 . . 4 4 . . . . 
+                    . . e e e e 4 4 . . 4 4 . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    `,img`
+                    2 2 2 4 2 2 2 2 2 2 2 2 2 . . . 
+                    . 4 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+                    2 2 2 4 4 4 2 4 2 4 4 4 2 4 4 4 
+                    2 4 4 4 4 4 4 4 4 4 4 4 4 2 4 4 
+                    . 4 2 4 4 4 f 4 4 4 f 4 4 2 2 . 
+                    . 2 2 4 4 4 4 4 4 4 4 4 4 . 2 2 
+                    2 2 . 4 4 4 4 f f f 4 4 4 . . . 
+                    . . . . . e e 4 4 4 4 e . . . . 
+                    . . . . 4 4 e 4 4 4 e 4 4 2 2 . 
+                    . e e 4 4 . 4 4 4 4 4 4 . 4 2 2 
+                    e e 4 4 e . 4 4 4 4 4 4 . 4 4 2 
+                    e e e e e e 4 4 4 4 4 4 . . . . 
+                    e e e e e e 4 4 . . 4 4 . . . . 
+                    . e e e e e 4 4 . . 4 4 . . . . 
+                    . . e e e e 4 4 . . 4 4 . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    `,img`
+                    2 2 2 4 2 2 2 2 2 2 2 2 2 . . . 
+                    . 4 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+                    2 2 2 4 4 4 2 4 2 4 4 4 2 4 4 4 
+                    2 4 4 4 4 4 4 4 4 4 4 4 4 2 4 4 
+                    . 4 2 4 4 4 f 4 4 4 f 4 4 2 2 . 
+                    . 2 2 4 4 4 4 4 4 4 4 4 4 . 2 2 
+                    2 2 . 4 4 4 4 f f f 4 4 4 . . . 
+                    . . . . . e e 4 4 4 4 e . . . . 
+                    . . . . 4 4 e 4 4 4 e 4 4 4 2 2 
+                    . e e 4 4 . 4 4 4 4 4 4 . 4 2 . 
+                    e e 4 4 e . 4 4 4 4 4 4 . 2 . . 
+                    e e e e e e 4 4 4 4 4 4 . . . . 
+                    e e e e e e 4 4 . . 4 4 . . . . 
+                    . e e e e e 4 4 . . 4 4 . . . . 
+                    . . e e e e 4 4 . . 4 4 . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    `],
+                500,
+                false
+                )
+            }
+            pause(1000)
+        }
+    }
+}
+scene.onOverlapTile(SpriteKind.water, sprites.dungeon.collectibleInsignia, function (sprite, location) {
+    info.changeScoreBy(100)
+    game.setGameOverMessage(true, "CONGRATS WATERBOY WIN")
+    effects.confetti.endScreenEffect()
+})
+let projectile3: Sprite = null
+let projectile2: Sprite = null
+let projeciel1: Sprite = null
+let list: Sprite[] = []
+let statusbar: StatusBarSprite = null
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+let fdsafds = sprites.createProjectileFromSprite(img`
+    ....888444888888................................
+    ..888444444488888...8888888888888888882222222...
+    ..8244444222222288888244444444444222224444444...
+    .88442442222222224444444444444444444445555555...
+    .82222224222222222885555888888888888888888888...
+    .82444442222222222288888855555555555555555555...
+    .8242222222222222225555555555555555555555555....
+    .8222222222222222225555555555555555555555555....
+    .8222222222222222225555555566666666666666666....
+    .88222222222222422256666666699999999999999999...
+    ..8222222222224666666999999999999999999999999...
+    ..8822222222666699999999996666666666666666966...
+    ...88222222246666666666666622222222222222666....
+    ....882222442668882.............................
+    .....882244888888...............................
+    ......888888888.................................
+    `, mySprite, 150, 0)
+let sadxczxc = sprites.createProjectileFromSprite(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . 8 . . . 
+    . . . . . . . . . 8 8 8 8 . . . 
+    . . . . 9 8 8 8 8 8 2 2 . . . . 
+    . . . 9 9 8 8 8 4 2 2 2 . . . . 
+    . . . 9 8 8 8 9 4 2 2 2 . . . . 
+    . . . 8 8 8 9 9 4 2 2 2 . . . . 
+    . . . 8 9 9 9 8 4 2 2 4 . . . . 
+    . . . 8 9 8 4 4 4 2 2 4 . . . . 
+    . . . 8 9 4 4 2 2 2 2 4 . . . . 
+    . . . . 8 4 4 2 2 2 4 4 . . . . 
+    . . . . . . 4 2 2 2 4 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, mySprite, 150, 0)
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -15,8 +370,8 @@ let mySprite = sprites.create(img`
     8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
     8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
     8 8 8 8 8 8 8 6 6 8 6 6 . . . . 
-    `, SpriteKind.Player)
-let mySprite2 = sprites.create(img`
+    `, SpriteKind.water)
+mySprite2 = sprites.create(img`
     2 2 2 4 2 2 2 2 2 2 2 2 2 . . . 
     . 4 2 2 2 2 2 2 2 2 2 2 2 2 . . 
     2 2 2 4 4 4 2 4 2 4 4 4 2 4 4 4 
@@ -33,9 +388,27 @@ let mySprite2 = sprites.create(img`
     . e e e e e 4 4 . . 4 4 . . . . 
     . . e e e e 4 4 . . 4 4 . . . . 
     . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(mySprite)
-controller.player2.moveSprite(mySprite)
+    `, SpriteKind.fire)
+let asdasd = sprites.createProjectileFromSprite(img`
+    ................................................
+    ....88888888888888888888888888888888888888888888
+    ...889999996666664444466666446666666666644444466
+    ..8994444499666666666466444444444444444446666222
+    ..8944444442222266666444466622222244222222222222
+    ..8222222244444222222222222224444444444444444444
+    ..8225555225555555555555555555555555555555555555
+    ..8225555525555555555555555555555555555555555555
+    ..8222555525555555555555555555555555555555555555
+    ..8922255224444444444444422222222222222222222222
+    ..8942222244444444442222229999999999999999999999
+    ..8944222222222222222999999999999999999999999999
+    ..8994444499999999999999999222222222222222222222
+    ...899444998999999999222222222222222222222222222
+    ....89999988888888888888888888888888888888888888
+    ....8888888.....................................
+    `, mySprite, 150, 0)
+controller.moveSprite(mySprite, 100, 0)
+controller.player2.moveSprite(mySprite2, 100, 0)
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.setBackgroundImage(img`
     bbbbbbbbbbbbbbccccbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccddddddddbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbccccbbbbcc
@@ -241,4 +614,15 @@ scene.setBackgroundImage(img`
     `)
 tiles.placeOnRandomTile(mySprite, sprites.dungeon.collectibleBlueCrystal)
 tiles.placeOnRandomTile(mySprite2, sprites.dungeon.collectibleRedCrystal)
-scene.cameraFollowSprite(mySprite)
+splitScreen.setSplitScreenEnabled(true)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, mySprite)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera2, mySprite2)
+splitScreen.setCameraRegion(splitScreen.Camera.Camera2, splitScreen.CameraRegion.HorizontalTopHalf)
+splitScreen.setCameraRegion(splitScreen.Camera.Camera1, splitScreen.CameraRegion.HorizontalBottomHalf)
+mySprite.ay = 300
+mySprite2.ay = 300
+info.setLife(1)
+statusbar = statusbars.create(20, 4, StatusBarKind.Energy)
+statusbar.max = 100
+statusbar.value = 50
+list = [sadxczxc, fdsafds, asdasd]
